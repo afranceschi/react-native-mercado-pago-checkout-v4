eb9c19975a896fd2e4c185a1218f12328bff86dc
Agustin Franceschi <agustin@MacBook-Air.local>
2020-04-09 22:58:55 -0300

    Add android support

--- android/src/main/java/com/reactlibrary/RNMercadoPagoCheckoutV4Module.java
+++ android/src/main/java/com/reactlibrary/RNMercadoPagoCheckoutV4Module.java
@@ -6,17 +6,40 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
+import com.mercadopago.android.px.core.MercadoPagoCheckout;
+
 public class RNMercadoPagoCheckoutV4Module extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
+  private String _publicKey;
+  private String _preferenceId;
+  private static final int REQUEST_CODE = 1;
 
   public RNMercadoPagoCheckoutV4Module(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
+    this._publicKey = "null";
+    this._preferenceId = "null";
   }
 
   @Override
   public String getName() {
     return "RNMercadoPagoCheckoutV4";
+  }
+
+  @ReactMethod
+  public void setPublicKey(String publicKey) {
+    this._publicKey = publicKey;
+  }
+
+  @ReactMethod
+  public void setPreferenceId(String preferenceId) {
+    this._preferenceId = preferenceId;
+  }
+
+  @ReactMethod
+  public void open() {
+    new MercadoPagoCheckout.Builder(this._publicKey, this._preferenceId).build()
+                    .startPayment(this.reactContext, this.REQUEST_CODE);
   }
 }
