008bf763b14547c6e7336b4ab6c8cbe8d8d379d4
Agustin Franceschi <agustin@MacBook-Air.local>
2020-04-10 18:34:55 -0300

    Add android events

--- android/src/main/java/com/reactlibrary/RNMercadoPagoCheckoutV4Module.java
+++ android/src/main/java/com/reactlibrary/RNMercadoPagoCheckoutV4Module.java
@@ -3,12 +3,22 @@ package com.reactlibrary;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
 
 import com.mercadopago.android.px.core.MercadoPagoCheckout;
+import com.mercadopago.android.px.model.Payment;
+import com.mercadopago.android.px.model.exceptions.MercadoPagoError;
 
-public class RNMercadoPagoCheckoutV4Module extends ReactContextBaseJavaModule {
+public class RNMercadoPagoCheckoutV4Module extends ReactContextBaseJavaModule implements ActivityEventListener{
 
   private final ReactApplicationContext reactContext;
   private String _publicKey;
@@ -17,6 +27,8 @@ public class RNMercadoPagoCheckoutV4Module extends ReactContextBaseJavaModule {
 
   public RNMercadoPagoCheckoutV4Module(ReactApplicationContext reactContext) {
     super(reactContext);
+    
+    reactContext.addActivityEventListener(this);
     this.reactContext = reactContext;
     this._publicKey = "null";
     this._preferenceId = "null";
@@ -35,11 +47,51 @@ public class RNMercadoPagoCheckoutV4Module extends ReactContextBaseJavaModule {
   @ReactMethod
   public void setPreferenceId(String preferenceId) {
     this._preferenceId = preferenceId;
+    Log.i("STATUS","PREFERENCEID");
   }
 
   @ReactMethod
   public void open() {
+    Activity activity = getCurrentActivity();
     new MercadoPagoCheckout.Builder(this._publicKey, this._preferenceId).build()
-                    .startPayment(this.reactContext, this.REQUEST_CODE);
+                    .startPayment(activity, this.REQUEST_CODE);
+  }
+
+
+  @Override
+  public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+      if(requestCode == this.REQUEST_CODE){
+        Log.i("STATUS",Integer.toString(resultCode));
+        if (resultCode == MercadoPagoCheckout.PAYMENT_RESULT_CODE) {
+          final Payment payment = (Payment) data.getSerializableExtra(MercadoPagoCheckout.EXTRA_PAYMENT_RESULT);
+          WritableMap send_data = Arguments.createMap();
+          send_data.putString("status", "FINISH");
+          send_data.putString("paymentId", Long.toString(payment.getId())); 
+          this.reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("checkout", send_data);
+          Log.i("STATUS", "Resultado del pago: " + payment.getPaymentStatus());
+            //Done!
+        } else if (resultCode == 0) {
+            if (data != null && data.getExtras() != null
+                && data.getExtras().containsKey(MercadoPagoCheckout.EXTRA_ERROR)) {
+                final MercadoPagoError mercadoPagoError =
+                    (MercadoPagoError) data.getSerializableExtra(MercadoPagoCheckout.EXTRA_ERROR);
+                Log.i("STATUS","Error: " +  mercadoPagoError.getMessage());
+                //Resolve error in checkout
+            } else {
+              //Resolve canceled checkout
+              WritableMap send_data = Arguments.createMap();
+              send_data.putString("status", "CANCEL");
+              this.reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("checkout", send_data);
+              Log.i("STATUS","CANCELED");
+            }
+        }
+      }
+      // Do nothing
+  }
+
+  @Override
+  public void onNewIntent(Intent intent) {
+    Log.i("STATUS","INTENT");
+      //sendJSEvent(intent);
   }
 }
--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 
-import { NativeModules } from 'react-native';
+import { NativeModules, NativeEventEmitter} from 'react-native';
 
 const { RNMercadoPagoCheckoutV4 } = NativeModules;
+const events = new NativeEventEmitter(RNMercadoPagoCheckoutV4);
+//export default RNMercadoPagoCheckoutV4;
 
-export default RNMercadoPagoCheckoutV4;
+export default {
+    setPublicKey: publicKey => RNMercadoPagoCheckoutV4.setPublicKey(publicKey),
+    setPreferenceId: preferenceId => RNMercadoPagoCheckoutV4.setPreferenceId(preferenceId),
+    open: () => RNMercadoPagoCheckoutV4.open(),
+    addListener: func => events.addListener("checkout", func),
+    remove: () => events.remove(),
+}
